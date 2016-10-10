a7b966ccf612ddf2426fb7941ae51f542c8675fa
Krishna Chandolu <chandolu@yahoo-inc.com>
2016-10-09 21:30:29 -0700

    Find all unique triplets in the array which gives the sum of zero.(3sum) problem

---
+++ unique_3sum_triplet.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+class Solution {
+  
+  
+  public ArrayList<ArrayList<Integer>> findTriplet(int[] arr) {
+    
+    Arrays.sort(arr);
+    
+    // Handle Duplicates by using HAshset for collecting the triplets in ArrayList
+    HashSet<ArrayList<Integer>> hs = new HashSet<ArrayList<Integer>>();
+    
+    for(int i =0 ; i< arr.length-2; i++){
+    
+      int j = i+1;
+      int k = arr.length-1;
+      
+      while (j < k){
+        int sum = arr[i]+arr[j]+arr[k];
+        
+        if (sum == 0) {
+          
+          ArrayList<Integer> inner = new ArrayList<Integer>();
+          
+          //System.out.println("Found : " + arr[i] +","+arr[j]+","+arr[k]);
+          inner.add(arr[i]);
+          inner.add(arr[j]);
+          inner.add(arr[k]);
+          hs.add(inner);
+          j++;
+          k--;
+          
+        }else if (sum > 0) {
+          k--;
+        } else if (sum < 0 ){
+          j++;
+        }
+      }
+    
+    }
+    return new ArrayList<ArrayList<Integer>>(hs);
+  
+  }
+  
+  
+  
+  
+  public static void main(String[] args) {
+    
+    Solution sol = new Solution();
+  
+    int[] inp = new int[]{-1, 0, 1, 2, -1, -4};
+    
+    System.out.println(sol.findTriplet(inp));
+    
+    
+    
+  }
+}
+
