d18c7acccf7977190acb20e7bbca94007ce99966
Krishna Chandolu <kchandolu@groupon.com>
2018-10-16 00:04:29 -0700

    Add time complexity for Merge k soreted lists using Priority Queue

--- mergeKSortedArray.java
+++ mergeKSortedArray.java
@@ -5,6 +5,18 @@ import java.util.*;
  * Using Priority Queue as Heap to merge K Sorted arrays
  */
 
+/*Complexity Analysis
+
+Time complexity : O(Nlogk) where k is the number of linked lists.
+
+The comparison cost will be reduced to O(logk) for every pop and insertion to priority queue. But finding the node with the smallest value just costs O(1)O(1) time.
+There are N nodes in the final linked list.
+Space complexity :
+
+O(n) Creating a new linked list costs O(n) space.
+O(k) The code above present applies in-place method which cost O(1) space. And the priority queue (often implemented with heaps) costs O(k) space (it's far less than NN in most situations)
+*/
+
 class Solution {
   
   class EachArray implements Comparable<EachArray> {
